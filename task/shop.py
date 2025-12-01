0be49b986a4485d37d0b0bc4d9f858ef1778df54
老王 <934247240@qq.com>
2025-12-01 20:37:52 +0800

    更新商店

--- task/shop.py
+++ task/shop.py
@@ -15,7 +15,34 @@ class ShopTask:
         util.press_keyboard('h')
         util.wait_image('return')
         # time.sleep(3)
-        util.wait_and_click_image("weekShop")
+        # 先尝试直接查找weekShop
+        found_week_shop = util.wait_image("weekShop", max_attempts=5)
+        if not found_week_shop:
+            # 如果没找到weekShop，尝试通过滚动来查找
+            start_time = time.time()
+            x, y = 180, 625
+            pyautogui.moveTo(x, y)
+            time.sleep(0.5)
+            
+            # 在一分钟内持续寻找
+            scroll_count = 0
+            while time.time() - start_time < 60:
+                if util.wait_image("weekShop", max_attempts=1):
+                    util.wait_and_click_image("weekShop")
+                    found_week_shop = True
+                    break
+                # 向下滚动一点继续寻找
+                pyautogui.scroll(-200)  # 向下滚动
+                scroll_count += 1
+                time.sleep(0.5)
+                
+            # 如果最终没有找到，直接返回主菜单
+            if not found_week_shop:
+                util.to_main_menu()
+                return
+        else:
+            util.wait_and_click_image("weekShop")
+        
         pyautogui.moveTo(1000, 500)
         time.sleep(1)
         pyautogui.scroll(-10000)
